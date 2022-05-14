a0905beb0e29f5f282087cda5712c23058b4e3d0
Callum <76244827+cjsxb@users.noreply.github.com>
2022-05-14 14:05:02 +1000

    Unity Project Upload

---
+++ Assets/Scripts/GhostMove.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GhostMove : MonoBehaviour
+{
+    public Transform waypointParent;
+    public float speed = 0.3f;
+    private Transform[] waypoints;
+    private int currentPoint = 0;
+
+    private void Start()
+    {
+        int childCount = waypointParent.childCount;
+        waypoints = new Transform[childCount];
+        for (int i = 0; i < childCount; i++)
+        {
+            waypoints[i] = waypointParent.GetChild(i);
+        }
+    }
+
+    bool Approximately(Vector2 a, Vector2 b)
+    {
+        return Mathf.Approximately(a.x, b.x) &&
+               Mathf.Approximately(a.y, b.y);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Waypoint not reached yet? Then move closer
+        Vector3 ghostPos = transform.position;
+        Vector3 waypointPos = waypoints[currentPoint].position;
+
+        if (!Approximately(ghostPos, waypointPos))
+        {
+            Vector2 position = Vector2.MoveTowards(ghostPos, waypointPos, speed);
+            GetComponent<Rigidbody2D>().MovePosition(position);
+        }
+
+        // Waypoint has been reached, select the next one
+        else currentPoint = (currentPoint + 1) % waypoints.Length;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.name == "pacman")
+        {
+            Pacdot.pacdotsCollected = 0;
+            Destroy(col.gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Pacdot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pacdot : MonoBehaviour
+{
+    public static int pacdotsCollected;
+
+    public Text text;
+
+    // Check if collided
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.name == "pacman")
+        {
+            if (pacdotsCollected == 329)
+            {
+                Time.timeScale = 0;
+                Debug.Log("330 Reached!");
+                text.text = "You Win!";
+                Destroy(gameObject);
+            }
+            else
+            {
+                pacdotsCollected = pacdotsCollected + 1;
+                Debug.Log(pacdotsCollected);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
